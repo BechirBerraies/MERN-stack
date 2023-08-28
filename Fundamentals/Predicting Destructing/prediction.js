4a7e2b12f577312ae481c47dfaa10f939d578543
BechirBerraies <136343630+BechirBerraies@users.noreply.github.com>
2023-08-28 18:08:43 +0100

    reorganize

--- Fundamentals/Predicting Destructing/prediction.js
+++ Fundamentals/Predicting Destructing/prediction.js
@@ -68,18 +68,18 @@
 
 // FUNCTION 5 
 
-const lastTest = {
-    key: 'value',
-    secondKey: [1, 5, 1, 8, 3, 3]
-}
-const { key } = lastTest;
-const { secondKey } = lastTest;
-const [ ,willThisWork] = secondKey;
-//Predict the output
-console.log(key);
-console.log(secondKey);
-console.log(secondKey[0]);
-console.log(willThisWork);
+// const lastTest = {
+//     key: 'value',
+//     secondKey: [1, 5, 1, 8, 3, 3]
+// }
+// const { key } = lastTest;
+// const { secondKey } = lastTest;
+// const [ ,willThisWork] = secondKey;
+// //Predict the output
+// console.log(key);
+// console.log(secondKey);
+// console.log(secondKey[0]);
+// console.log(willThisWork);
 
 
 // output 
@@ -94,3 +94,38 @@ console.log(willThisWork);
 //[1,5,1,1,8,3,3]
 //1
 //5
+
+
+
+
+
+
+// const person = {
+//     firstName: 'Bob',
+//     lastName: 'Marley',
+//     email: 'bob@marley.com',
+//     password: 'sekureP@ssw0rd9',
+//     username: 'barley',
+//     addresses: [
+//       {
+//         address: '1600 Pennsylvania Avenue',
+//         city: 'Washington, D.C.',
+//         zipcode: '20500',
+//       },
+//       {
+//         address: '221B Baker St.',
+//         city: 'London',
+//         zipcode: 'WC2N 5DU',
+//       }
+//     ],
+//     createdAt: 1543945177623
+//   };
+
+//   const { firstName, lastName, ...attributes } = person;
+//   console.log(attributes);
+
+
+
+// const square = n => n * n;
+// console.log(square(5));
+
